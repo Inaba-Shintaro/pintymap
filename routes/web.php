fe61a32964c9aee9f9bb5be4a04229d961a26f20
Inaba Shintaro <inaba.shintaro.6221@gmail.com>
2022-06-07 15:26:10 +0900

    to add login & guestLogin function

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login/guest', [LoginController::class, 'guestLogin']);
 
 Auth::routes();
 
