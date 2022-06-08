8b9a7e61be04c38df2c479ff1dd771745675b95a
Inaba Shintaro <inaba.shintaro.6221@gmail.com>
2022-06-08 18:36:30 +0900

    to commentOut postSeeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -15,33 +15,33 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('posts')->insert([
-            'title' => '東京タワー',
-            'description' => 'これはタイトル1の説明文です',
-            'lat' => 35.65881,
-            'lng' => 139.74544,
-            'user_id' => 1,
-        ]);
-        DB::table('posts')->insert([
-            'title' => 'スカイツリー',
-            'description' => 'これはタイトル2の説明文です',
-            'lat' => 35.71008,
-            'lng' => 139.81070,
-            'user_id' => 1,
-        ]);
-        DB::table('posts')->insert([
-            'title' => '上野公園',
-            'description' => 'これはタイトル3の説明文です',
-            'lat' => 35.71569,
-            'lng' => 139.77083,
-            'user_id' => 2,
-        ]);
-        DB::table('posts')->insert([
-            'title' => '平和記念公園',
-            'description' => 'これはタイトル4の説明文です',
-            'lat' => 26.09511,
-            'lng' => 127.72347,
-            'user_id' => 2,
-        ]);
+        // DB::table('posts')->insert([
+        //     'title' => '東京タワー',
+        //     'description' => 'これはタイトル1の説明文です',
+        //     'lat' => 35.65881,
+        //     'lng' => 139.74544,
+        //     'user_id' => 1,
+        // ]);
+        // DB::table('posts')->insert([
+        //     'title' => 'スカイツリー',
+        //     'description' => 'これはタイトル2の説明文です',
+        //     'lat' => 35.71008,
+        //     'lng' => 139.81070,
+        //     'user_id' => 1,
+        // ]);
+        // DB::table('posts')->insert([
+        //     'title' => '上野公園',
+        //     'description' => 'これはタイトル3の説明文です',
+        //     'lat' => 35.71569,
+        //     'lng' => 139.77083,
+        //     'user_id' => 2,
+        // ]);
+        // DB::table('posts')->insert([
+        //     'title' => '平和記念公園',
+        //     'description' => 'これはタイトル4の説明文です',
+        //     'lat' => 26.09511,
+        //     'lng' => 127.72347,
+        //     'user_id' => 2,
+        // ]);
     }
 }
