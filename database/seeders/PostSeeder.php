e8411aff91bf7c6040db61f82410618c56294221
Inaba Shintaro <inaba.shintaro.6221@gmail.com>
2022-06-08 18:10:55 +0900

    to test postSeeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -15,33 +15,33 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('posts')->insert([
-            'title' => '東京タワー',
-            'description' => 'これはタイトル1の説明文です',
-            'lat' => '35.65881584518403',
-            'lng' => '139.7454436267236',
-            'user_id' => 1,
-        ]);
-        DB::table('posts')->insert([
-            'title' => 'スカイツリー',
-            'description' => 'これはタイトル2の説明文です',
-            'lat' => '35.710083953151084',
-            'lng' => '139.81070391582526',
-            'user_id' => 1,
-        ]);
-        DB::table('posts')->insert([
-            'title' => '上野公園',
-            'description' => 'これはタイトル3の説明文です',
-            'lat' => '35.71569347326716',
-            'lng' => '139.77083424012167',
-            'user_id' => 2,
-        ]);
-        DB::table('posts')->insert([
-            'title' => '平和記念公園',
-            'description' => 'これはタイトル4の説明文です',
-            'lat' => '26.095112065206223',
-            'lng' => '127.72347587232395',
-            'user_id' => 2,
-        ]);
+        // DB::table('posts')->insert([
+        //     'title' => '東京タワー',
+        //     'description' => 'これはタイトル1の説明文です',
+        //     'lat' => '35.65881584518403',
+        //     'lng' => '139.7454436267236',
+        //     'user_id' => 1,
+        // ]);
+        // DB::table('posts')->insert([
+        //     'title' => 'スカイツリー',
+        //     'description' => 'これはタイトル2の説明文です',
+        //     'lat' => '35.710083953151084',
+        //     'lng' => '139.81070391582526',
+        //     'user_id' => 1,
+        // ]);
+        // DB::table('posts')->insert([
+        //     'title' => '上野公園',
+        //     'description' => 'これはタイトル3の説明文です',
+        //     'lat' => '35.71569347326716',
+        //     'lng' => '139.77083424012167',
+        //     'user_id' => 2,
+        // ]);
+        // DB::table('posts')->insert([
+        //     'title' => '平和記念公園',
+        //     'description' => 'これはタイトル4の説明文です',
+        //     'lat' => '26.095112065206223',
+        //     'lng' => '127.72347587232395',
+        //     'user_id' => 2,
+        // ]);
     }
 }
