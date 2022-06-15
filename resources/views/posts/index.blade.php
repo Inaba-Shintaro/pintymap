6fab7aa9935164b6894f81a2ff1e9de680e7f597
Inaba Shintaro <inaba.shintaro.6221@gmail.com>
2022-06-15 16:40:21 +0900

    to add function for search in indexPage

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,6 +5,14 @@
 @auth
 @include('layouts.pageHeader',['pageHeader' => 'ポスト一覧画面'])
 
+<form method="GET" action="{{ route('post.index') }}">
+  <input name="search" value="@if (isset($search)) {{ $search }} @endif" class="form-control" type="text" placeholder="キーワードで検索" aria-label="default input example">
+  <div class="d-flex">
+    <button type="submit" class="btn btn-primary">検索</button>
+    <a class="btn btn-primary" href="{{ route('post.index') }}" role="button">クリア</a>
+  </div>
+</form>
+
 <div class="row">
   @foreach ($posts as $post)
   <div class="col-md-4 col-12 mb-4">
@@ -23,7 +31,25 @@
     </div>
   </div>
   @endforeach
+
+  @isset($users)
+  @foreach ($users as $user)
+  <div class="col-md-4 col-12 mb-4">
+    <div class="card">
+      @isset ($user->image)
+      <img src="{{$user->image}}" class="card-img-top cardImage" alt="...">
+      @else
+      <img src="{{asset('storage/images/neko.jpeg')}}" class="card-img-top cardImage" alt="...">
+      @endisset
+      <div class="card-body">
+        <a href="{{ route('mypage', ['user' => $user->id]) }}" class="btn btn-primary btn-rounded showBtn text-lowercase" role="button">詳細を見る</a>
+      </div>
+    </div>
+  </div>
+  @endforeach
+  @endisset
+
 </div>
 @endauth
-{{ $posts->links() }}
+{{ $posts->appends(request()->input())->links() }}
 @endsection
