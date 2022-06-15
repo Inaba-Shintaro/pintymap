e4e735d53fae8cb887e2c47592274806e7f5cf92
Inaba Shintaro <inaba.shintaro.6221@gmail.com>
2022-06-15 17:12:24 +0900

    to use same loopInstance in index & mypage

---
+++ resources/views/posts/loopItem.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-4 col-12 mb-4">
+  <div class="card">
+    @isset ($post->image)
+    <img src="{{$post->image}}" class="card-img-top cardImage" alt="...">
+    @else
+    <img src="{{asset('storage/images/neko.jpeg')}}" class="card-img-top cardImage" alt="...">
+    @endisset
+    <div class="card-body">
+      <h5 class="card-title">{{$post->title}}</h5>
+      <p class="card-text">{{$post->description}}</p>
+      @include('layouts.userIcon', ['record' => $post->user])
+      <a href="{{ route('post.show', ['post' => $post->id]) }}" class="btn btn-primary btn-rounded showBtn text-lowercase" role="button">詳細を見る</a>
+    </div>
+  </div>
+</div>
