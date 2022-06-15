37a656fb116177d68be39e6be97cf978032ce772
Inaba Shintaro <inaba.shintaro.6221@gmail.com>
2022-06-15 17:19:43 +0900

    to make description less than 30chracters

--- resources/views/posts/loopItem.blade.php
+++ resources/views/posts/loopItem.blade.php
@@ -7,7 +7,7 @@
     @endisset
     <div class="card-body">
       <h5 class="card-title">{{$post->title}}</h5>
-      <p class="card-text">{{$post->description}}</p>
+      <p class="card-text">{{\Illuminate\Support\Str::limit($post->description, 60, '...')}}</p>
       @include('layouts.userIcon', ['record' => $post->user])
       <a href="{{ route('post.show', ['post' => $post->id]) }}" class="btn btn-primary btn-rounded showBtn text-lowercase" role="button">詳細を見る</a>
     </div>
